c0091c2f4ade93e88ce6c1bdcd3485d4350bb632
Michael A <m.accetto@massarius.com>
2022-04-19 15:49:56 +0200

    Azure Devops Secret as Environment Variable in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from az_devops_items import collect_work_items
 from bq import load_df_to_bq
+import os
 
 def sync_devops_to_bq(*args):
    work_items_df = collect_work_items(*args)
@@ -9,5 +10,6 @@ def sync_devops_to_bq(*args):
 if __name__ == '__main__':
    # _token variable to be assigned from secret.
    _url = 'https://dev.azure.com/Massarius-Adtech'
+   _token = os.environ['GITHUB_AZURE_INTEGRATION_READONLY']
 
    sync_devops_to_bq(_url, _token)
